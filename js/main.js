fd7a0677718ba7437d8b83f9a909be2ff058e8e7
chenyong <chen_yong@sinosoft.com.cn>
2021-01-17 20:03:42 +0800

    右侧固定

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@
 $('.colse_pop').click(function () {
     $(this).parents('.pop_mask').hide()
 })
+//页面滚动到顶部
+$('.go_top').click(function () {
+    $("html,body").animate({
+        scrollTop: 0
+    }, 500);
+})
